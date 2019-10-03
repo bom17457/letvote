36e39cd618f598580f0f0675ec89163587809ffe
Akaporn <brombom43531@gmail.com>
2019-10-03 16:58:53 +0700

    update app

--- app.js
+++ app.js
@@ -11,14 +11,21 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var versionRouter = require('./routes/version');
-
+var signin = require('./routes/signin');
+var ballot = require('./routes/ballot');
 var app = express();
-
+app.use((req, res, next) => {    
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
+    next();
+})
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/version', versionRouter.router);
-
+app.use('/signin', signin.router)
+app.use('/ballot', ballot.router)
 module.exports = app;
