a85dc32c1f240f8e6e9955d84f3225adcebffa2b
Akaporn <brombom43531@gmail.com>
2019-10-04 12:35:13 +0700

    update createbaseimage

--- createbaseimage.sh
+++ createbaseimage.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-scp ./Dockerfile.base jenkins:~/backend/Dockerfile.base
-scp ./package.json jenkins:~/backend/package.json
-ssh jenkins docker build -f /root/backend/Dockerfile.base -t backend:0.0.1 .
+if [ "$1" == "app" ] || [ "$1" == "" ]
+then
+    scp ./Dockerfile.base jenkins:~/backend/Dockerfile.base
+    scp ./package.json jenkins:~/backend/package.json
+    ssh jenkins docker build -f /root/backend/Dockerfile.base -t backend:0.0.1 .
+elif [ $1 == "e2e" ]
+then
+    scp ./Dockerfile.e2e.base jenkins:~/backend/e2e/Dockerfile.e2e.base
+    ssh jenkins docker build -f /root/backend/e2e/Dockerfile.e2e.base -t e2e:0.0.1 .
+fi
