165278d409934a102afd56cd27e38803ac975597
Akaporn <brombom43531@gmail.com>
2019-10-04 21:57:36 +0700

    change name

---
+++ test/unitTest/authTest.js
@@ -0,0 +1,13 @@
+let assert = require('assert')
+let mock = require('mock-express-response')
+let signin = require('../../middlewares/authen')
+describe('login logout',async function(){
+    let req = { get: function(){}}
+    let res = new mock({})
+    let next = {};
+
+    await 
+    it('should return http 200 and token access', async function(){
+        
+    })
+})
--- test/unitTest/signin_outTest.js
+++
@@ -1,8 +0,0 @@
-let assert = require('assert')
-let mock = require('mock-express-response')
-
-describe('login logout', function(){
-    it('should return http 200 and token access', async function(){
-        
-    })
-})
