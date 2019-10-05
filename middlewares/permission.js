a25088dc63d78f6264ad7ab7d6257b6c11918867
Akaporn <brombom43531@gmail.com>
2019-10-06 05:51:36 +0700

    change name

---
+++ middlewares/permission.js
@@ -0,0 +1,16 @@
+this.roles = []
+
+function auth(req, res, next){
+    const {role} = req.body
+    if(this.roles.includes(role)){
+        next()
+    }else{
+        return res.sendStatus(401)
+    }
+}
+
+
+module.exports = function(roles){
+    this.roles = roles
+    return auth
+}
