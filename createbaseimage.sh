76c027ec2b08636d89cae92b5613804551964230
Akaporn <brombom43531@gmail.com>
2019-10-06 20:01:12 +0700

    update version

--- createbaseimage.sh
+++ createbaseimage.sh
@@ -4,7 +4,7 @@ if [ "$1" == "app" ] || [ "$1" == "" ]
 then
     scp ./Dockerfile.unit.base jenkins:~/backend/Dockerfile.unit.base
     scp ./package.json jenkins:~/backend/package.json
-    ssh jenkins docker build -f /root/backend/Dockerfile.unit.base -t backend:0.0.1 .
+    ssh jenkins docker build -f /root/backend/Dockerfile.unit.base -t backend:$version .
 elif [ $1 == "e2e" ]
 then
     scp ./Dockerfile.e2e.base jenkins:~/backend/e2e/Dockerfile.e2e.base
