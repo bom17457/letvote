441d8363ffad19730d196745789540c67a5e4713
Akaporn <brombom43531@gmail.com>
2019-10-07 13:36:22 +0700

    update query

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ var db = require('./db.js')
 var passwordEncode = require('../utilities/passwordEncoder')
 
 module.exports.findUser = function(username) {
-    var user = db.query(`SELECT * from student WHERE username='${username}' LIMIT 1`)
+    var user = db.query(`SELECT * from user WHERE username='${username}' LIMIT 1`)
         .then(function (result) {     
             if(result.length == 0) throw error
             return result[0]            
