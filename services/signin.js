ffd65c388d4d44ba7478d96b0092a8c47f8254c5
Akaporn <brombom43531@gmail.com>
2019-10-07 23:53:00 +0700

    fixed exp

--- services/signin.js
+++ services/signin.js
@@ -7,12 +7,19 @@ let moment = require('moment')
 
 module.exports = {
     signin: async function (username, password) {
-        let user = await userdb.findUsername(username)
-        if (await passwordEncode.matchPassword(password, user.password)) {
-            user.exp = moment().add(config.expire, 'days').format('YYYY-MM-DD HH:MM:SS')
-            let token = jwt.encode(user, config.scretKey)
-            log_login.login(token, user.id)            
-            
+        const user = await userdb.findUsername(username)
+        if (await passwordEncode.matchPassword(password, user.password)) {            
+            let token = jwt.encode({
+                id:user.id,
+                username: user.username,
+                fname: user.fname,
+                lname: user.lname,
+                role: user.role,
+                status: user.status,
+                iat: moment().unix(),
+                exp: moment().add(config.expire, 'days').unix()
+            }, config.scretKey) 
+            log_login.login(token, user.id)                        
             return {token: token}
         } else {
             throw 'not match password'
