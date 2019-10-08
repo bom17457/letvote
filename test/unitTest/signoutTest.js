752b3426a072374fa14c8a8594345334d48c4973
Akaporn <brombom43531@gmail.com>
2019-10-08 12:33:14 +0700

    add signoutTest

--- test/unitTest/signoutTest.js
+++ test/unitTest/signoutTest.js
@@ -1,13 +1,17 @@
 let assert = require('assert')
 let mockResponse = require('mock-express-response')
 let mockRequest = require('mock-express-request')
-let signin = require('../../controllers/signout')
+let signout = require('../../controllers/signout')
+
 describe('signout', function(){
 
     it('Should return 200', async function(){
         let req = new mockRequest({
-            header: {
-                "AccessToken": "Barer sometoken"
+            headers: {
+                "authorization": "Bearer sometoken"
+            },
+            authInfo: {
+                id: '025930461038-1'
             }
         })
         let res = new mockResponse({})
@@ -15,8 +19,27 @@ describe('signout', function(){
         let exec = false
         let next = function(){exec = true};
         
-        await signin.signin(req, res, next)        
+        await signout.signout(req, res, next)        
         await assert.equal(res.statusCode, '200', 'Should return 200')     
+        await assert.notEqual(res.statusCode, '401', 'Should not return 401')
     })
 
+    it('Should return 401', async function(){
+        let req = new mockRequest({
+            headers: {
+                "authorizations": "Bearer sometoken"
+            },
+            authInfo: {
+                id: '025930461038-1'
+            }
+        })
+        let res = new mockResponse({})
+
+        let exec = false
+        let next = function(){exec = true};
+        
+        await signout.signout(req, res, next)        
+        await assert.equal(res.statusCode, '401', 'Should return 401')     
+        await assert.notEqual(res.statusCode, '200', 'Should not return 200')
+    })
 })
