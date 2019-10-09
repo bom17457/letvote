74d881e6e89a7a19da8ae3f317f2953cd82431be
Akaporn <brombom43531@gmail.com>
2019-10-10 01:23:25 +0700

    test create election

---
+++ test/unitTest/manageElectionTest.js
@@ -0,0 +1,56 @@
+let assert = require('assert')
+let mockResponse = require('mock-express-response')
+let mockRequest = require('mock-express-request')
+let manageElection = require('../../controllers/manageElection')
+
+describe('signout', function () {
+
+    it('Should return 200', async function () {
+        let req = new mockRequest({
+            authInfo: {
+                id: '025930461038-1'
+            },
+            body: {
+                topic: 'Student Election 2020',
+                description: 'Electing',
+                electionFrom: '2019-09-22 11:01:50',
+                electionTo: '2019-09-22 11:01:50',
+                registerFrom: '2019-09-22 11:01:50',
+                registerTo: '2019-09-22 11:01:50',
+                displayText: 'Who is your partant'
+            }
+        })
+        let res = new mockResponse({})
+
+        let exec = false
+        let next = function () { exec = true };
+
+        await manageElection.PostElection(req, res, next)
+        await assert.equal(res.statusCode, '201', 'Should return 201')
+        await assert.notEqual(res.statusCode, '400', 'Should not return 400')
+    })
+
+    it('Should return 401', async function () {
+        let req = new mockRequest({
+            authInfo: {
+                id: '025930461038-1'
+            },
+            body: {
+                topic: 'Student Election 2020',
+                description: 'Electing',
+                electionFrom: '18082019',
+                electionTo: '18082019',
+                registerFrom: '18082019',
+                registerTo: '18082019',
+                displayText: 'Who is your partant'
+            }
+        })
+        let res = new mockResponse({})
+
+        let exec = false
+        let next = function () { exec = true };
+
+        await manageElection.PostElection(req, res, next)
+        await assert.equal(res.statusCode, '400', 'Should not return 400')
+    })
+})
