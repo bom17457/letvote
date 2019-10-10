37363769b08b3330a692b8bebecb7f5633bfa7aa
Akaporn <brombom43531@gmail.com>
2019-10-11 00:07:15 +0700

    test getElection function

--- test/unitTest/manageElectionTest.js
+++ test/unitTest/manageElectionTest.js
@@ -54,3 +54,16 @@ describe('Create Election', function () {
         await assert.equal(res.statusCode, '400', 'Should not return 400')
     })
 })
+
+describe('getAllElection', function () {
+    it('should return 200 and return json object', async function () {
+        let req = new mockRequest({})
+        let res = new mockResponse({})
+        let exec = false
+        let next = function () { exec = true }
+
+        await manageElection.getElection(req, res, next)
+        await assert.equal(res.statusCode, '200', 'Should return 200')
+        await assert.equal(typeof res._getJSON(), 'object', 'Should return array datatype')
+    })
+})
