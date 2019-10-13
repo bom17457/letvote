e1a3ea9e9f9ab29667234bd7118d3d1a39d91c89
Akaporn <brombom43531@gmail.com>
2019-10-13 17:00:13 +0700

    make search user

--- controllers/getUserProperties.js
+++ controllers/getUserProperties.js
@@ -4,7 +4,7 @@ var IUserProperties = require('../services/userProperties.js')
 
 const struct = {
   getProperties: async function (req, res, next) {    
-    const {id} = req.authInfo    
+    const {id} = req.authInfo
     try{
       let user = await IUserProperties.properties(id)
       res.json(user)
@@ -12,8 +12,18 @@ const struct = {
       res.send(401, {message:Exception.toString()})
     }
   },
+  searchUser: async function (req, res, next) {
+    try {
+        const { id, fullname } = req.body
+        let users = await IUserProperties.searchUser(id, fullname)
+        res.json(users)
+    } catch (Exception) {        
+        res.send(400, { message: Exception.toString() })
+    }
+},
 }
 router.get('/', struct.getProperties);
+router.post('/search', struct.searchUser)
 module.exports = {
   router: router,
   ...struct
--- services/userProperties.js
+++ services/userProperties.js
@@ -1,9 +1,27 @@
 let users = require('../models/user.model')
-
+let Sequelize = require('sequelize')
+let Op = Sequelize.Op
 module.exports = {
     properties: async function (id) {
-        let userdetail = await users.findAll({where:{id:id}})
-        if(userdetail.length == 0) throw 'not found'
+        let userdetail = await users.findAll({ where: { id: id } })
+        if (userdetail.length == 0) throw 'not found'
         return userdetail[0]
+    },
+    searchUser: async function (id, fullname) {
+        let userdetail = await users.findAll(
+            {
+                where: {
+                    [Op.or]: [Sequelize.where(Sequelize.fn("concat", Sequelize.col("fname"), " ", Sequelize.col("lname")), {
+                        [Op.like]: `%${fullname}%`
+                    }), 
+                    { 
+                        id: id 
+                    }]
+                }
+            })
+        
+        if (userdetail.length == 0) throw 'not found'
+
+        return userdetail
     }
 }
