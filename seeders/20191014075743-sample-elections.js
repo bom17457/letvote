d6c985db458c79cf2bc1c09bbaff38ed419d5616
Akaporn <brombom43531@gmail.com>
2019-10-31 10:42:08 +0700

    add id 3 sample election data

--- seeders/20191014075743-sample-elections.js
+++ seeders/20191014075743-sample-elections.js
@@ -27,6 +27,7 @@ module.exports = {
       create_datetime: new Date(),
       owner: '025930461038-1'
     }, {
+      id: 3,
       topic: 'topic Title3',
       description: 'election description 2',
       displaytext: 'display',
