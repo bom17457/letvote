d6dddbebcc497c73df9116820b54a5efbfdd1a1a
Akaporn <brombom43531@gmail.com>
2019-11-03 01:21:12 +0700

    check user login

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,6 +1,7 @@
 let passport = require('passport')
 let passportJWT = require('passport-jwt')
 let config = require('../config.json')
+const db = require('../models')
 
 const ExtractJwt = passportJWT.ExtractJwt
 const jwtOption = {
@@ -9,8 +10,15 @@ const jwtOption = {
 }
 
 const JwtStrategy = passportJWT.Strategy
-const howToAuth = new JwtStrategy(jwtOption, async function(payload, done) {  
+const howToAuth = new JwtStrategy(jwtOption, async function (payload, done) {
     done(null, true, payload)
 })
+
+const isLogin = async function (req, res, next) {
+    let found = await db.login.findOne({where:{token: req.headers.authorization, status: 'login'}})
+    if(found == null) res.send(405)    
+    next();
+}
+
 passport.use(howToAuth)
-module.exports = passport.authenticate("jwt", {session:false})
+module.exports = [passport.authenticate("jwt", { session: false }), isLogin]
